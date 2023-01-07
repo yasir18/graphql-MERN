53bf082d3f9def8b32477f7127d1c5f976a018de
Yasir <yasiraffan9@gmail.com>
2023-01-07 15:56:34 -0700

    graphql queries with dummy data

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
 const schema = require("./schema/schema");
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 8000;
 const app = express();
 
-// app.use('/graphql',graphqlHTTP({
-//     schema,
-//     graphiql: true
-// }))
+app.use(cors());
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.get("/test", function (req, res) {
+  res.status(200);
+  res.send("Hello world");
+});
 
 app.listen(port, console.log(`Server is listening at ${port}`));
